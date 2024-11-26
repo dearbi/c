8e15f95a1de734573abafab0b891b431ee426205
bi-wenbo <2199463351@qq.com>
2024-11-26 18:32:55 +0800

    的

--- 算法level-1/快速排序.cpp
+++ 算法level-1/快速排序.cpp
@@ -1,7 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void quick_sort(int q[],int l,int r)
+{
+    if(l>=r)
+        return;
+    int i=l-1,j=r+1,x=q[l+r>>1];
+    while(i<j)
+    {
+        do
+            i++;
+        while(q[i]<x);
+        do
+            j--;
+        while(q[j]>x);
+        if(i<j)
+            swap(q[i],q[j]);
+    }
+    quick_sort(q,l,j);
+    quick_sort(q,j+1,r);
+
+}
+
 int main()
 {
+    int n;
+    cin>>n;
+    int q[10010];
+    for(int i=0;i<n;i++)
+    {
+        cin>>q[i];
+    }
+
+    quick_sort(q,0,n-1);
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<q[i]<<" ";
+    }
 
 
     return 0;

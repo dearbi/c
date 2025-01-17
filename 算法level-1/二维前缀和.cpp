ec454c656f7c8375b4c95feac84864be5075331a
dear_bi <2199463351@qq.com>
2025-01-17 09:59:08 +0800

    Commit message

--- 算法level-1/二维前缀和.cpp
+++ 算法level-1/二维前缀和.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 const int MAX_M = 1000;
 const int MAX_N = 1000;
-int a[MAX_M + 1][MAX_N + 1];
 int sum[MAX_M + 1][MAX_N + 1];
 
 int main() {
@@ -14,27 +13,21 @@ int main() {
 
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                cin >> a[i][j];
-            }
-        }
-
-        memset(sum, 0, sizeof(sum));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                sum[i][j] = a[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+                cin>>sum[i][j];
+                sum[i][j] = sum[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
             }
         }
         int res = 0;
-        for (int i = 1; i + x - 1 <= m; i++) {
-            for (int j = 1; j + y - 1 <= n; j++) {
-                int l = i + x - 1;
-                int r = j + y - 1;
-                int temp = sum[l][r] - sum[i - 1][r] - sum[l][j - 1] + sum[i - 1][j - 1];
+        for (int i = 1; i <= m-x+1; i++) {
+            for (int j = 1; j <= n-y+1; j++) {
+                int x2 = i + x - 1;
+                int y2 = j + y - 1;
+                int temp = sum[x2][y2] - sum[i - 1][y2] - sum[x2][j - 1] + sum[i - 1][j - 1];
                 res = max(res, temp);
             }
         }
         cout << res << endl;
     }
-
     return 0;
 }
+
--- 题目/草稿.cpp
+++ 题目/草稿.cpp
@@ -1,45 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int N = 1e5;
-int a[N], temp[N];
-long long count_inversions = 0;
-
-void mergeSort(int left, int right) {
-    if (left >= right) return;
-
-    int mid = (left + right) / 2;
-
-    mergeSort(left, mid);
-    mergeSort(mid + 1, right);
-
-    int i = left, j = mid + 1, k = left;
-    while (i <= mid && j <= right) {
-        if (a[i] <= a[j]) {
-            temp[k++] = a[i++];
-        } else {
-            temp[k++] = a[j++];
-            count_inversions += (mid - i + 1);
-        }
-    }
-
-    while (i <= mid) temp[k++] = a[i++];
-    while (j <= right) temp[k++] = a[j++];
-
-    for (int i = left; i <= right; i++) {
-        a[i] = temp[i];
-    }
-}
+const int MAX_M = 1000;
+const int MAX_N = 1000;
+int sum[MAX_M + 1][MAX_N + 1];
 
 int main() {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
+    int t;
+    cin >> t;
+    while (t--) {
+        int m, n, x, y;
+        cin >> m >> n >> x >> y;
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                cin>>sum[i][j];
+                sum[i][j] = sum[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+        int res = 0;
+        for (int i = 1; i + x - 1 <= m; i++) {
+            for (int j = 1; j + y - 1 <= n; j++) {
+                int l = i + x - 1;
+                int r = j + y - 1;
+                int temp = sum[l][r] - sum[i - 1][r] - sum[l][j - 1] + sum[i - 1][j - 1];
+                res = max(res, temp);
+            }
+        }
+        cout << res << endl;
     }
-
-    mergeSort(1, n);
-
-    cout << count_inversions << endl;
     return 0;
 }
